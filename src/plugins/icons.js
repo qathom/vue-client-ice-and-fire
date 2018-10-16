a3fd44ebb40009d1e399262d9a64097f5a1c60f3
Qathom <tony9295@gmail.com>
2018-10-17 00:02:27 +0200

    Add icons

---
+++ src/plugins/icons.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import Icon from 'vue-awesome/components/Icon';
+
+// import icons
+import 'vue-awesome/icons/book';
+import 'vue-awesome/icons/user';
+import 'vue-awesome/icons/archway';
+import 'vue-awesome/icons/arrow-left';
+
+Vue.component('icon', Icon);
